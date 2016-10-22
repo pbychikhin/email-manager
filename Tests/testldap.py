d5b83c2e9784a6a108484c6454c1b89034bdb0c6
Pavel Bychikhin <pbychikhin@yahoo.com>
2016-10-22 14:48:42 +0300

    added cidict type from python-ldap. this is obligatory for correct processing implemented domain data retrieving

--- Tests/testldap.py
+++ Tests/testldap.py
@@ -1,5 +1,6 @@
 
-import argparse, random, ldap
+import argparse, random, ldap, sys
+from ldap.cidict import cidict
 
 cmdlnparser = argparse.ArgumentParser(description="Get users from AD")
 cmdlnparser.add_argument("-c", help="AD controller(s)", action="append")
@@ -10,10 +11,26 @@ cmdlargs = cmdlnparser.parse_args()
 lobj = ldap.initialize("ldap://{}".format(random.choice(cmdlargs.c)))
 lobj.set_option(ldap.OPT_PROTOCOL_VERSION, ldap.VERSION3)
 
-lres = lobj.search_s(base="", scope=ldap.SCOPE_BASE,
-                     attrlist=("defaultNamingContext", "configurationNamingContext",
-                               "domainFunctionality", "serverName",
-                               "dnsHostName"))
-rootDSE = lres[0][1].items()
-for attrname, attrvalue in rootDSE:
-    print "Attr {} is {}".format(attrname, attrvalue[0])
+# Get RootDSE
+lres = lobj.search_s(base="",
+                     scope=ldap.SCOPE_BASE,
+                     attrlist=("defaultNamingContext", "configurationNamingContext", "domainFunctionality",
+                               "serverName", "dnsHostName"))
+rootDSE = cidict(lres[0][1])
+
+# Bind
+try:
+    lobj.bind_s(cmdlargs.u, cmdlargs.p)
+except ldap.INVALID_CREDENTIALS:
+    print "The credentials supplied are invalid"
+    sys.exit(1)
+
+# Get domain
+lres = lobj.search_s(base="CN=Partitions,{}".format(rootDSE["configurationNamingContext"][0]),
+                     scope=ldap.SCOPE_ONELEVEL,
+                     filterstr="(&(objectClass=crossRef)(nCName={}))".format(rootDSE["defaultNamingContext"][0]),
+                     attrlist=("dnsRoot", "nETBIOSName"))
+domainAttrs = cidict(lres[0][1])
+print "Domain attrs:"
+for name, val in domainAttrs.items():
+    print "Attr {}: {}".format(name, val[0])
